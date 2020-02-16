21fb2720b8616ba0265e12309c64a947f67cd420
Ray <raymondk0917@gmail.com>
2020-02-15 20:37:39 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const name = document.getElementById('name').value
+const email = document.getElementById('email').value
+const event = document.getElementById('event').value
+const date = document.getElementById('date').value
+
+var arr = document.URL.match(/room=([0-9]+)/)
+var room = arr[1];
+
+
+
